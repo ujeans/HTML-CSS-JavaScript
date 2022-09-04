2adf20c24a1b2af80c448bbe9a7f9127ed4f0c7d
ujin <dbwlsxkal95@gmail.com>
2022-09-04 22:06:54 +0900

    vanillaJS 함수, if, for문 공부

---
+++ vanillaJS/javascript.js
@@ -0,0 +1,30 @@
+문자열 = "", ', ``
+숫자 = 0, 1.3
+배열 = []
+Boolean = true, false
+Object = {}
+function()
+undefined
+null
+
+if(!false) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+for(let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+const arr = [1, 2, 3]
+arr.forEach()
+
+const fn = function() {} // 익명 함수
+
+function fn() {} // 이름 있는 함수
+
+const fn = () => {} // arrow 함수
+
+fn()
+
